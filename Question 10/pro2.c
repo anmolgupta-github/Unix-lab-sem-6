faa2f72fc6e7f508c77510ab385ce681859d852c
anmolgupta-github <anmolgupta1478@gmail.com>
2026-06-09 14:22:11 +0530

    tenthquestion

--- Question 10/pro2.c
+++ Question 10/pro2.c
@@ -1,37 +1,38 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<stdlib.h>
-#include<sys/wait.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main()
 {
-	int pd1,pd2;
+    pid_t pid1, pid2;
 
-	pd1 = fork();
-	if(pd1 == 0)
-	{
-		printf("First pid :%d\n",getpid());
-		sleep(1);
-		printf("First wait\n");
-		return 0;
-	}
-	else{
-		pd2 = fork();
-		if(pd2 == 0)
-		{
-			printf("Second pid :%d\n", getpid());
-			sleep(2);
-			printf("Second wait\n");
-			return 0;
-		}
-		else{
-			printf("first pid :%d\n", getpid());
+    pid1 = fork();
 
-			wait(NULL);
-			waitpid(pd2, NULL, 0);
-		}
-	}
-	 return 0;
+    if(pid1 == 0)
+    {
+        printf("Child 1 PID: %d\n", getpid());
+        sleep(2);
+        printf("Child 1 exiting\n");
+        return 0;
+    }
+
+    pid2 = fork();
+
+    if(pid2 == 0)
+    {
+        printf("Child 2 PID: %d\n", getpid());
+        sleep(4);
+        printf("Child 2 exiting\n");
+        return 0;
+    }
+
+    printf("Parent PID: %d\n", getpid());
+
+    wait(NULL);
+    printf("Parent: One child terminated using wait()\n");
+
+    waitpid(pid2, NULL, 0);
+    printf("Parent: Child 2 terminated using waitpid()\n");
+
+    return 0;
 }
